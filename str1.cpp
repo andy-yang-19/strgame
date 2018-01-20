5bd5b6bfc21a43b2f930f7374d6dd7c4e33948f6
Andy Yang <312691663@qq.com>
2018-01-21 01:03:30 +0800

    change algorithm

--- str1.cpp
+++ str1.cpp
@@ -1,21 +1,33 @@
 #include <pthread.h>
 #include <iostream>
 #include <string>
+#include <sys/time.h>
+#include <signal.h>
 #include "fifo.h"
 #include "strproc.h"
 #include "err.h"
 
 using namespace std;
 
+static bool run = true;
+static size_t prodCnt = 0;
+static size_t consCnt = 0;
+
+void sig_handler(int sig)
+{
+    run = false;
+}
+
 struct ThreadArg
 {
     int cnt;
 };
 
-Fifo<string> fifo;
+static Fifo<string> fifo;
 
 void *producer(void *arg)
 {
+#if 0
     int cnt = ((ThreadArg*)arg)->cnt;
 
     while(cnt-- > 0) {
@@ -23,12 +35,20 @@ void *producer(void *arg)
         //cout << "p: " << *str << endl;
         fifo.put(str);
     }
+#else
+    while(run) {
+        shared_ptr<string> str = genStr();
+        fifo.put(str);
+        prodCnt++;
+    }
+#endif
 
     return NULL;
 }
 
 void *consumer(void *arg)
 {
+#if 0
     int cnt = ((ThreadArg*)arg)->cnt;
 
     while(cnt-- > 0) {
@@ -36,6 +56,13 @@ void *consumer(void *arg)
         sortStr(*str);
         //cout << "c: " << *str << endl;
     }
+#else
+    while(run) {
+        shared_ptr<string> str = fifo.get();
+        sortStr(*str);
+        consCnt++;
+    }
+#endif
 
     return NULL;
 }
@@ -46,6 +73,7 @@ int main(int argc, char *argv[])
     ThreadArg threadArgs[2];
     int e;
 
+#if 0
     int cnt = 10;
     if(argc > 1) {
         cnt = atoi(argv[1]);
@@ -53,6 +81,23 @@ int main(int argc, char *argv[])
 
     threadArgs[0].cnt = cnt;
     threadArgs[1].cnt = cnt;
+#else
+    int secs = 2;
+    if(argc > 1)
+        secs = atoi(argv[1]);
+
+    struct itimerval itv;
+    itv.it_interval.tv_sec = 0;
+    itv.it_interval.tv_usec = 0;
+    itv.it_value.tv_sec = secs;
+    itv.it_value.tv_usec = 0;
+
+    if(setitimer(ITIMER_REAL, &itv, NULL) < 0) {
+        perror("setitimer");
+        return -1;
+    }
+    signal(SIGALRM, sig_handler);
+#endif
 
     e = pthread_create(&tidp, NULL, &producer, &threadArgs[0]);
     if(e)
@@ -69,6 +114,8 @@ int main(int argc, char *argv[])
     e = pthread_join(tidc, NULL);
     if(e)
         handle_err(e, "pthread_join");
+
+    cout << "prod: " << prodCnt << ", cons: " << consCnt << endl;
 
     return 0;
 }
--- str2.cpp
+++ str2.cpp
@@ -1,10 +1,20 @@
 #include <cstdlib>
 #include <iostream>
+#include <sys/time.h>
+#include <signal.h>
 #include "strproc.h"
 using namespace std;
 
+static bool run = true;
+
+void sig_handler(int sig)
+{
+    run = false;
+}
+
 int main(int argc, char* argv[])
 {
+#if 0
     int loopCnt = 10;
     if(argc > 1)
         loopCnt = atoi(argv[1]);
@@ -15,5 +25,33 @@ int main(int argc, char* argv[])
         sortStr(ss);
         //cout << *s << "," << ss << endl;
     }
+#else
+    int secs = 2;
+    if(argc > 1)
+        secs = atoi(argv[1]);
+
+    struct itimerval itv;
+    itv.it_interval.tv_sec = 0;
+    itv.it_interval.tv_usec = 0;
+    itv.it_value.tv_sec = secs;
+    itv.it_value.tv_usec = 0;
+
+    if(setitimer(ITIMER_REAL, &itv, NULL) < 0) {
+        perror("setitimer");
+        return -1;
+    }
+    signal(SIGALRM, sig_handler);
+
+    int cnt = 0;
+    while(run) {
+        shared_ptr<string> s = genStr();
+        string ss = *s;
+        sortStr(ss);
+        cnt++;
+        //cout << *s << "," << ss << endl;
+    }
+    cout << "cnt: " << cnt << endl;
+
+#endif
 }
 
