b5d73ca67202a583bf738039c31eac199519886f
Andy Yang <312691663@qq.com>
2018-01-21 12:13:17 +0800

    report

--- test.sh
+++ test.sh
@@ -5,14 +5,14 @@ ncpu=`nproc`
 str1file=c${ncpu}s1
 str2file=c${ncpu}s2
 
-rm ${str1file} ${str2file}
+rm -f ${str1file} ${str2file}
 
-for i in {1..10}
+for i in {1..1}
 do
 	./str1 10 >> ${str1file}
 done
 
-for i in {1..10}
+for i in {1..1}
 do
 	./str2 10 >> ${str2file}
 done
